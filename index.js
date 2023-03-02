8350d35097ea90d9f498c08c0d86c40cebb4da11
David Chu Kuong Io <david@marstree.ltd>
2023-03-02 16:47:49 +0800

    init project

---
+++ index.js
@@ -0,0 +1,24 @@
+module.exports.option = (event) => {
+    return {
+        method: event.httpMethod,
+        path: event.path,
+        data: {
+            ...(event.body ? JSON.parse(event.body) : {}),
+            ...(event.queryStringParameters ? event.queryStringParameters : {}),
+        },
+        headers: event.headers
+    }
+}
+
+
+module.exports.response = (data, statusCode = 200) => {
+    return {
+        statusCode: (!!data) ? statusCode : 400,
+        //  Uncomment below to enable CORS requests
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "*"
+        },
+        body: JSON.stringify(data)
+    }
+}
